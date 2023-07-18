7390e4db2d8405553fecd9362f0935ddbb390f17
vorobily <ilya.vorobev3@gmail.com>
2023-07-18 14:19:56 +0200

    Initial commit

---
+++ src/test/java/com/example/airlinesreservation/AirlinesReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.airlinesreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlinesReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
