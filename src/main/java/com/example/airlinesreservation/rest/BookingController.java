da33808c11a79a4b31bc6e18ddea9d65f22a6274
vorobily <ilya.vorobev3@gmail.com>
2023-07-21 10:11:49 +0200

    Booking function finally work, basic html pages ready

--- src/main/java/com/example/airlinesreservation/rest/BookingController.java
+++ src/main/java/com/example/airlinesreservation/rest/BookingController.java
@@ -1,61 +1,68 @@
 package com.example.airlinesreservation.rest;
 
 import java.time.LocalDate;
-import java.util.List;
-
-import com.example.airlinesreservation.entity.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.stereotype.Controller;
 
 import com.example.airlinesreservation.entity.Booking;
 import com.example.airlinesreservation.entity.Flight;
 import com.example.airlinesreservation.exception.FlightException;
 import com.example.airlinesreservation.service.BookingService;
 import com.example.airlinesreservation.service.FlightService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
-
-
-@CrossOrigin()
 @Controller
 public class BookingController {
 
 	@Autowired
 	private BookingService bookservice;
-	
+
 	@Autowired
 	private FlightService flightservice;
 
+	@GetMapping("/booking")
+	public String showBookingPage(Model model, @RequestParam(required = false) int flightId,
+								  @RequestParam String source,
+								  @RequestParam String destination,
+								  @RequestParam String date) {
+
+		Flight flight = flightservice.fetchById(flightId);
+
+		// Create a Booking object with pre-filled flight information
+		Booking booking = new Booking();
+		booking.setFlight(flight);
+		// Set other default values for the booking if needed
+
+		// Add the booking object to the model
+		model.addAttribute("booking", booking);
+
+		return "booking"; // Assuming your booking.html is in the "templates" directory
+	}
+
 	@PostMapping("/booking")
-	public ResponseEntity<?> addBooking(@RequestBody Booking booking,
-										@RequestParam int flightId,
-										@RequestParam String source,
-										@RequestParam String destination,
-										@RequestParam String date) throws FlightException {
+	public String addBooking(@ModelAttribute("booking") Booking booking,
+							 @RequestParam int flightId,
+							 String source,
+							 String destination,
+							 String date, Model model) throws FlightException {
 
 		Flight flight = flightservice.fetchById(flightId);
 		if (flight.getAvailableSeats() <= 0) {
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Seats are not available");
+			model.addAttribute("message", "Seats are not available");
+			return "Seats are not available";
 		} else if (booking.getNumberOfSeatsToBook() > flight.getAvailableSeats()) {
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Only " + flight.getAvailableSeats() + " seats are available");
+			model.addAttribute("message", "Only " + flight.getAvailableSeats() + " seats are available");
+			return "Only "+flight.getAvailableSeats()+" are Available";
 		} else {
 			flight.setAvailableSeats(flight.getAvailableSeats() - booking.getNumberOfSeatsToBook());
 			flightservice.updateFlight(flight);
 			booking.setFlight(flight);
 			booking.setBookingDate(LocalDate.now());
 			int bid = bookservice.addBooking(booking);
-			return ResponseEntity.ok(bid);
+			model.addAttribute("bookingId", bid);
+			return "redirect:/";
 		}
 	}
 }
